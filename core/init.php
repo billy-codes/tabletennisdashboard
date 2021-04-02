bfdc4daef358b3caae5e19b7bb8a2d6f51d968ad
Billy <billy.codes@gmail.com>
2021-04-03 01:17:31 +0800

    Code added to connect to database

--- core/init.php
+++ core/init.php
@@ -1,4 +1,14 @@
 <?php
+// Connecting to mySQL Database using mysqli_connect();
 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'tabletennis');
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($link === false){
+    die("Error: Could not connect to database");
+}
 
 ?>
