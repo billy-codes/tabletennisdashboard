35c270b07848d02553f206f21b7ebe2a7112fefd
Billy <billy.codes@gmail.com>
2021-04-07 12:51:52 +0800

    Initial Commit for players profile page

---
+++ profile.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Table Tennis Statistics</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+  <!--Main Navigation-->
+  <header>
+
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark black scrolling-navbar">
+      <div class="container">
+
+        <!-- Brand -->
+        <a class="navbar-brand waves-effect" href="localhost/login.php" target="_blank">
+          <strong class="blue-text">TableTennis Stats</strong>
+        </a>
+
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <!-- Left -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="localhost/tournaments.php" target="_blank">
+                Tournaments
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="localhost/players.php" target="_blank">
+                Players
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="localhost/matches.php" target="_blank">
+                Matches
+              </a>
+            </li>
+          </ul>
+
+          <!-- Right -->
+          <ul class="navbar-nav nav-flex-icons">
+              <?php 
+                if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+                  $current_user = $_SESSION["username"];
+              ?>
+              <li class="nav-item">
+                <a href="panel.php" class="nav-link border border-light rounded waves-effect" target="_blank">
+                  <i class="fas fa-user mr-2"></i>Welcome <?php echo $_SESSION["name"];?>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="logout.php" class="nav-link">
+                  Logout
+                </a>
+              </li>
+              <?php }else {?>
+              <li class="nav-item">
+                <a href="login.php" class="nav-link border border-light rounded waves-effect" target="_blank">
+                  <i class="fas fa-user mr-2"></i>Login
+                </a>
+              </li>
+              <?php }?>
+          </ul>
+
+        </div>
+
+      </div>
+    </nav>
+    <!-- Navbar -->
+
+  </header>
+  <!--Main Navigation-->
+
+  <!--Main layout-->
+  <main class="mt-5 pt-5">
+    <div class="container">
+      <!--Section: Cards-->
+        <section class="text-center">
+           
+        </section>
+      <!--Section: Cards-->
+    </div>
+  </main>
+  <!--Main layout-->
+
+  <!--Footer-->
+  <footer class="page-footer text-center font-small mdb-color darken-2 mt-4 wow fadeIn">
+
+    <!--Copyright-->
+    <div class="footer-copyright py-3">
+      © 2020 Copyright:
+      <a href="index.php" target="_blank"> Belal Khaled </a>
+    </div>
+    <!--/.Copyright-->
+
+  </footer>
+  <!--/.Footer-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+
+  </script>
+</body>
+
+</html>
